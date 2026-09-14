fcdfd2fca18aaf198e746dbcdd489320addb615e
vathozdev <271198640+vathozdev@users.noreply.github.com>
2026-09-14 21:25:13 +0300

    feat: add sidebar with create folder button to DOM structure

---
+++ src/dom.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container");
+const sideBar = document.createElement("div");
+sideBar.classList.add("side-bar");
+const createFolderBtn = document.createElement("button");
+createFolderBtn.classList.add("create-folder");
+createFolderBtn.textContent = "Create a new folder";
+sideBar.appendChild(createFolderBtn);
+const list = document.createElement("ul");
+sidebar.appendChild(list);
+container.appendChild(sideBar);
