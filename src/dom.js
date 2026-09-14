b5c8e087b44ef37f5249c3c5f686c88c13a2e790
vathozdev <271198640+vathozdev@users.noreply.github.com>
2026-09-14 22:39:17 +0300

    feat: add expense form and button to the main area for expense tracking

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,6 @@
 import { Folder, FolderManager } from "./data.js";
 
-console.log("DOM JS loaded");
+console.log("DOM JS loaded"); //temporary
 const container = document.querySelector(".container");
 const sideBar = document.createElement("div");
 sideBar.classList.add("side-bar");
@@ -11,9 +11,45 @@ sideBar.appendChild(createFolderBtn);
 const list = document.createElement("ul");
 sideBar.appendChild(list);
 container.appendChild(sideBar);
+
 const mainArea = document.createElement("div");
 mainArea.classList.add("main-area");
+const addExpenseBtn = document.createElement("button");
+addExpenseBtn.classList.add("expense-btn");
+addExpenseBtn.textContent = "Add Expense";
+mainArea.appendChild(addExpenseBtn);
+
+const form = document.createElement("form");
+form.classList.add("expense-form");
+const title = document.createElement("input");
+title.type = "text";
+title.placeholder = "Title";
+form.appendChild(title);
+
+const amountContainer = document.createElement("div");
+amountContainer.classList.add("amount-container");
+
+const currencySign = document.createElement("span");
+currencySign.innerText = "$"; 
+currencySign.classList.add("currency-sign");
+
+const amount = document.createElement("input");
+amount.type = "number";
+amount.placeholder = "0.00";
+amount.step = "0.01";
+amount.min = "0";
+
+amountContainer.appendChild(currencySign);
+amountContainer.appendChild(amount);
+form.appendChild(amountContainer);
+
+
+
 container.appendChild(mainArea);
+
+addExpenseBtn.addEventListener("click", () => {
+
+})
 
 const folderManager = new FolderManager();
 
